a46c114f37f51a171bcf6f580970e01cc474622f
Matteo Manili <M.Manili.esterno@almaviva.it>
2023-09-19 05:36:56 +0200

    Primo commit iniziale

---
+++ src/main/java/com/lunasapiens/LunasapiensApplication.java
@@ -0,0 +1,26 @@
+package com.lunasapiens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LunasapiensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunasapiensApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String homePage(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("HOME PAGE", name);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/lunasapiens/LunasapiensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunasapiens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunasapiensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
