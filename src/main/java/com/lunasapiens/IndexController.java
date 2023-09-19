2acaecc05e89dae6949c845e58a532c45837d797
Matteo Manili <M.Manili.esterno@almaviva.it>
2023-09-19 21:58:51 +0200

    pagina web

--- src/main/java/com/lunasapiens/IndexController.java
+++ src/main/java/com/lunasapiens/IndexController.java
@@ -4,16 +4,24 @@ package com.lunasapiens;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "Benvenuto!");
-        // Puoi eseguire operazioni o passare dati al model qui se necessario
-        return "index"; // Assumi che "index" sia il nome del tuo template HTML (senza estensione)
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
     }
+
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
 
     @GetMapping("/index")
     public String index_2(Model model) {
--- src/main/java/com/lunasapiens/LunasapiensApplication.java
+++ src/main/java/com/lunasapiens/LunasapiensApplication.java
@@ -21,4 +21,5 @@ public class LunasapiensApplication {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
 }
