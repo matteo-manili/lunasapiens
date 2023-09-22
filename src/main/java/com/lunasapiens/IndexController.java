e175bf51d73c17ad43dcc2e3141b9bc6711c8cc1
Matteo Manili <M.Manili.esterno@almaviva.it>
2023-09-22 10:01:14 +0200

    classi astrologiche

--- src/main/java/com/lunasapiens/IndexController.java
+++ src/main/java/com/lunasapiens/IndexController.java
@@ -34,7 +34,6 @@ public class IndexController {
     public String oroscopo(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 
 
-
         // ottiene un'istanza di oroscopo
         // TextHoroscop horoscop = new TextHoroscop ();
         // imposta    l'oroscopo dell'algoritmo di calcolo della posizione del pianeta desiderato . setPlanet ( nuovo PianetaAA0 ());
@@ -47,31 +46,22 @@ public class IndexController {
         // fa qualcosa con i dati, ad esempio genera dati grezzi System . fuori . println ( oroscopo . toString ());
 
 
-
         // get a horoscop instance
         final TextHoroscop horoscop = new TextHoroscop();
-
         // set your desired planet position calculation algorithm
         horoscop.setPlanet(new PlanetAA0());
-
         // set your desired house system calculation algorithm
         // may be anything from the at.kugel.zodiac.house package.
         horoscop.setHouse(new HousePlacidus());
-
         // set your user data time value
         horoscop.setTime(23, 01, 1981, 12);
-
         // set your user data location value
-        // Pisa 43.7 e 10.4
-        // Roma 49.9 e 12.4
+        // Pisa 43.7 e 10.4 --- Roma 49.9 e 12.4
         horoscop.setLocationDegree(49.9, 12.4);
-
         // calculate the values
         horoscop.calcValues();
-
         // do something with the data or output raw data
         System.out.println(horoscop.toString());
-
 
 
 
