ce0140df8da38a7bb07ef0588f5ccf79ca7cfd22
Matteo Manili <M.Manili.esterno@almaviva.it>
2023-10-14 03:32:56 +0200

    tik tok impl...

--- src/main/java/com/lunasapiens/TikTokController.java
+++ src/main/java/com/lunasapiens/TikTokController.java
@@ -1,14 +1,13 @@
 package com.lunasapiens;
 
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,41 +16,24 @@ import java.nio.file.Files;
 @Controller
 public class TikTokController {
 
-    @Autowired
-    private AppConfig appConfig;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-
-
 
     @GetMapping({"/videos", "/videos/"})
-    public ResponseEntity<byte[]> downloadFileTikTok() throws IOException {
-        // Carica il file TXT dal percorso specificato, ad esempio dalla directory delle risorse della tua applicazione
+    public ResponseEntity<byte[]> downloadFile() throws IOException {
+
+        // Carica il file TXT dalla directory delle risorse
         String fileName = "tiktokeVXnQAYg9OQQA35O7IuERbDCTSG5ICWV";
-        Resource resource = new ClassPathResource("static/"+fileName + ".txt");
+        Resource resource = new ClassPathResource("static/"+fileName+".txt");
+        byte[] fileData = Files.readAllBytes(resource.getFile().toPath());
 
-        // Controlla se il file esiste
-        if (resource.exists()) {
-            // Leggi il contenuto del file
-            byte[] fileContent = Files.readAllBytes(resource.getFile().toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+fileName+".txt");
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData(fileName, fileName + ".txt");  // Specifica il nome del file quando viene scaricato
-
-            return ResponseEntity.ok()
-                    .headers(headers)
-                    .body(fileContent);
-        } else {
-            // Se il file non esiste, restituisci una risposta 404
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(fileData.length)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(fileData);
     }
-
-
-
 
 
 }
