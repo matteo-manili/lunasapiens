791fbd3a4eba04d9cbd04f46928d9a7d6b3dab2f
Matteo Manili <M.Manili.esterno@almaviva.it>
2024-02-08 23:09:17 +0100

    generazione video

--- src/main/java/com/lunasapiens/VideoGenerator.java
+++ src/main/java/com/lunasapiens/VideoGenerator.java
@@ -7,14 +7,15 @@ import javax.imageio.ImageIO;
 
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import org.bytedeco.ffmpeg.global.avcodec;
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.Java2DFrameConverter;
+
 
 
 @Component
