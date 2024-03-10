364c5effab0946781d4c434f679df18b0b87c88c
Matteo Manili <M.Manili.esterno@almaviva.it>
2024-03-10 06:32:06 +0100

    generazione video e prove  di tiktok

--- src/main/java/com/lunasapiens/ScheduledTasks.java
+++ src/main/java/com/lunasapiens/ScheduledTasks.java
@@ -42,12 +42,20 @@ public class ScheduledTasks {
 
     // (* secondi * minuti * ore * giorno del mese * mese * giorno della settimana)
     // settato per le 23:50 ogni giorno: "0 50 23 * * *"
-    // settato per le 00:50 ogni giorno: "0 5 0 * * *"
-    @Scheduled(cron = "0 5 0 * * *", zone = "Europe/Rome")
+    // settato per le 00:05 ogni giorno: "0 5 0 * * *"
+    @Scheduled(cron = "0 1 0 * * *", zone = "Europe/Rome")
     public void executeTask() {
         creaOroscopoGiornaliero();
-        telegramBotClient.inviaMessaggio("Eseguito! ScheduledTasks.executeTask() "+ LocalDateTime.now());
-        logger.info("Task eseguito alle " + LocalDateTime.now());
+        telegramBotClient.inviaMessaggio("executeTask Eseguito! ScheduledTasks.executeTask() "+ Util.getNowRomeEurope());
+        logger.info("executeTask eseguito alle " + Util.getNowRomeEurope());
+    }
+
+    // eseguo 2 volte il task, perché lascia sempre qualche video a null
+    @Scheduled(cron = "0 5 0 * * *", zone = "Europe/Rome")
+    public void executeTask_2() {
+        creaOroscopoGiornaliero();
+        telegramBotClient.inviaMessaggio("executeTask_2 Eseguito! ScheduledTasks.executeTask() "+ Util.getNowRomeEurope());
+        logger.info("executeTask_2 eseguito alle " + Util.getNowRomeEurope());
     }
 
 
