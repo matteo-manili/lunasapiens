5ae47dc8b1c25d5aadcefdba2c3d614b5db28c94
matteo-manili <matteo.manili@gmail.com>
2024-07-12 02:14:29 +0200

    pagina contatti

--- src/main/resources/static/util.js
+++ src/main/resources/static/util.js
@@ -6,28 +6,9 @@
  * - Confronta l'URL della pagina corrente con l'URL di ciascun link nel menu.
  * - Aggiunge la classe 'active' al link corrispondente se l'URL coincide, evidenziandolo.
  */
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    // Ottieni l'URL della pagina corrente
-    var currentUrl = window.location.href;
-    // Seleziona tutti i link nel menu
-    var menuLinks = document.querySelectorAll(".navbar-nav a.nav-link");
-    // Itera sui link del menu
-    menuLinks.forEach(function(link) {
-        // Ottieni l'URL del link
-        var linkUrl = link.href;
-        // Confronta l'URL del link con l'URL della pagina corrente
-        if (currentUrl === linkUrl) {
-            // Aggiungi la classe 'active' al link corrente
-            link.classList.add("active");
-        }
-    });
-});
-*/
 document.addEventListener("DOMContentLoaded", function() {
     var currentUrl = window.location.href;
     var menuLinks = document.querySelectorAll(".navbar-nav a.nav-link");
-
     menuLinks.forEach(function(link) {
         var linkUrl = link.href;
         if (currentUrl === linkUrl) {
