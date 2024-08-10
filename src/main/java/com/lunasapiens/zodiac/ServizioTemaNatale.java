649b5a800c0f12bee31746e4c424af8a2f0a6edd
matteo-manili <matteo.manili@gmail.com>
2024-08-10 09:05:07 +0200

    ai 1 grado tema natale

--- src/main/java/com/lunasapiens/zodiac/ServizioTemaNatale.java
+++ src/main/java/com/lunasapiens/zodiac/ServizioTemaNatale.java
@@ -33,7 +33,7 @@ public class ServizioTemaNatale {
 
 
     // tokensRisposta signfiica i token da aggiungere oltre i token per la domanda
-    private final Double temperature = 0.5; private final Integer tokensAggiuntiPerRisposta = 200; private final Double caratteriPerTokenStima = 6.0;
+    private final Double temperature = 0.1; private final Integer tokensAggiuntiPerRisposta = 200; private final Double caratteriPerTokenStima = 6.0;
 
 
 
