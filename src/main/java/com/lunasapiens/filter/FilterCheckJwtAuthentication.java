c696dda1fe2a21f9644b72c983d5d76af0e90d16
matteo-manili <matteo.manili@gmail.com>
2024-08-28 22:35:32 +0200

    varie

--- src/main/java/com/lunasapiens/filter/FilterCheckJwtAuthentication.java
+++ src/main/java/com/lunasapiens/filter/FilterCheckJwtAuthentication.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,7 +25,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Optional;
-import java.util.UUID;
 
 
 /**
