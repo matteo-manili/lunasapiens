549447f93085e91f7eb4313ac8e648ea70941250
matteo-manili <matteo.manili@gmail.com>
2025-10-14 23:25:16 +0200

    conversione prima domanda chat

--- src/main/resources/static/js/snippetVisualizzazioneChatPsicologo.js
+++ src/main/resources/static/js/snippetVisualizzazioneChatPsicologo.js
@@ -1,2 +1,15 @@
 <!-- Event snippet for Visualizzazione di pagina conversion page -->
 gtag('event', 'conversion', {'send_to': 'AW-1011317208/-UdhCPzO0akbENjzneID'});
+
+function gtag_report_conversion(url) {
+    var callback = function () {
+        if (typeof(url) != 'undefined') {
+            window.location = url;
+        }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-1011317208/MmU3CLiyra0bENjzneID',
+        'event_callback': callback
+    });
+    return false;
+}
